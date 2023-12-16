6043500a7ffad12b14fa6489998a535dfd30bd12
Luis Alberto Nores Villaverde <luisnoresv@outlook.com>
2023-12-16 15:07:41 -0500

    feat(ui): handling errors

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,10 +1,15 @@
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import Form from '@/app/ui/invoices/edit-form';
+import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string; }; }) {
   const { id } = params;
   const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()]);
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
